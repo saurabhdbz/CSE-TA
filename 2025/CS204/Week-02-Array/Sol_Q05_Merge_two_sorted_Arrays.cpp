81d97ad5c88ee5e91ea50cc531928663d884d66a
saurabhdbz <70940589+saurabhdbz@users.noreply.github.com>
2025-08-20 08:48:11 +0530

    Update Sol_Q05_Merge_two_sorted_Arrays.cpp

--- 2025/CS204/Week-02-Array/Sol_Q05_Merge_two_sorted_Arrays.cpp
+++ 2025/CS204/Week-02-Array/Sol_Q05_Merge_two_sorted_Arrays.cpp
@@ -1,34 +1,48 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main() {
     int n1, n2;
     cin >> n1;
-    vector<int> arr1(n1);
-    for (int i = 0; i < n1; i++) cin >> arr1[i];
+
+    int arr1[n1];
+    for (int i = 0; i < n1; i++) {
+        cin >> arr1[i];
+    }
 
     cin >> n2;
-    vector<int> arr2(n2);
-    for (int i = 0; i < n2; i++) cin >> arr2[i];
+    int arr2[n2];
+    for (int i = 0; i < n2; i++) {
+        cin >> arr2[i];
+    }
 
-    vector<int> merged;
-    int i = 0, j = 0;
+    int merged[n1 + n2]; // final merged array
+    int i = 0, j = 0, k = 0;
 
-    // Merge process
+    // Merge process (two-pointer technique)
     while (i < n1 && j < n2) {
         if (arr1[i] <= arr2[j]) {
-            merged.push_back(arr1[i++]);
+            merged[k++] = arr1[i++];
         } else {
-            merged.push_back(arr2[j++]);
+            merged[k++] = arr2[j++];
         }
     }
 
-    // Copy remaining
-    while (i < n1) merged.push_back(arr1[i++]);
-    while (j < n2) merged.push_back(arr2[j++]);
+    // Copy remaining elements of arr1
+    while (i < n1) {
+        merged[k++] = arr1[i++];
+    }
 
-    // Print result
-    for (int x : merged) cout << x << " ";
+    // Copy remaining elements of arr2
+    while (j < n2) {
+        merged[k++] = arr2[j++];
+    }
+
+    // Print merged array
+    for (int x = 0; x < n1 + n2; x++) {
+        cout << merged[x] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
